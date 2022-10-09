b1f596c662e2df1a9533835816f30ef9aa533349
patatattak <patatattak@gmail.com>
2022-10-09 05:22:05 +0200

    test

---
+++ data.js
@@ -0,0 +1,57 @@
+var jsonData = {
+	language: "fr",
+	students: [
+		{
+			firstName: "Jean",
+			lastName: "Dupont",
+			countryCode: "FR",
+			age: "28 ans",
+			photoPath: "images/avatar1.png",
+			instrument: "Clarinette, clarinette basse",
+			description: "Dans ma famille, nous avions l'habitude de chanter des chansons au coin du feu, etc. Dans la région rurale où nous vivions, la connexion Internet était mauvaise. J'ai donc joué la musique que nous avions sur CD et ce que mes parents chantaient : Folk irlandais, Bob Dylan et Bob Marley."		
+		},
+		{
+			firstName: "Marcia",
+			lastName: "Gomes",
+			countryCode: "ES",
+			age: "22 ans",
+			photoPath: "images/avatar2.png",
+			description: "Plays guitar, piano and sings. Loves rock !!!"		
+		},
+		{
+			firstName: "Julio",
+			lastName: "Mama",
+			countryCode: "IT",
+			age: "22 ans",
+			photoPath: "images/avatar3.png",
+			description: "Plays guitar, piano and sings. Loves rock !!!"		
+		},
+		{
+			firstName: "Marguerite",
+			lastName: "Spieler",
+			countryCode: "DE",
+			age: "22 ans",
+			photoPath: "images/avatar4.png",
+			description: "Plays guitar, piano and sings. Loves rock !!!"		
+		},
+		{
+			firstName: "Ronald",
+			lastName: "Inho",
+			countryCode: "BR",
+			age: "22 ans",
+			photoPath: "images/avatar5.png",
+			description: "J'ai grandi à Paris dans un quartier populaire et multiculturel. Alors je me sens un peu de plusieurs cultures. Quand j'ai voyagé au Maroc, je me suis sentie chez moi, c'était familier. Mais en Bulgarie, au Portugal aussi. Et ailleurs..."		
+		},
+		{
+			firstName: "Micheline",
+			lastName: "Cantalou",
+			countryCode: "FR",
+			age: "22 ans",
+			photoPath: "images/avatar6.png",			
+			instrument: "Violoncelle eleiueiueoiueo eoiuoeiuoeieu eoieuoiueoieueoi eoieueoieueieoiu",
+			description: "J'ai grandi à Paris dans un quartier populaire et multiculturel. Alors je me sens un peu de plusieurs cultures. Quand j'ai voyagé au Maroc, je me suis sentie chez moi, c'était familier. Mais en Bulgarie, au Portugal aussi. Et ailleurs... </br>J'ai découvert le violoncelle à un spectacle, dans un tout petit théâtre de Paris où mon père m'avait emmenée. Il y avait un violoncelliste et une danseuse de flamenco. Ce spectacle m'a transportée. J'ai décidé que je voulais apprendre le violoncelle.",		
+			linkUrl: "youtube.com",
+			linkName: "Une super chanson..."
+		}
+	]
+}
