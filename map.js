aac1a0e7c3063bdd65ffb7d4a961aee29c4c0faf
patatattak <patatattak@gmail.com>
2024-06-18 05:10:47 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,90 @@
+function docWheel(e){
+	if(e.deltaY < 0){    
+		zoomIn();
+	}
+	else{    
+		zoomOut();
+	}	
+}
+
+function zoomIn(){	
+	if(state == "nav"){
+		let center = {
+			x: currentViewBox.left + currentViewBox.width / 2,
+			y: currentViewBox.top + currentViewBox.height / 2
+		};
+		let rect = mapImage.getBoundingClientRect();
+		let ratio = rect.width / rect.height;
+		console.log(ratio);
+		let newWidth = currentViewBox.width - currentViewBox.width * zoomStrength;
+		//let newHeight = currentViewBox.height - currentViewBox.height * zoomStrength;
+		
+		
+		if(newWidth < 0.1 * fullViewBox.width){
+			newWidth = 0.1 * fullViewBox.width;
+		}
+		let newHeight = newWidth / ratio;
+		if(newHeight < 0.1 * fullViewBox.height){
+			newHeight = 0.1 * fullViewBox.height;
+			newWidth = newHeight * ratio;
+		}
+		if(newHeight > 670){
+			newHeight = 670;
+		}
+		currentViewBox = {
+			left: center.x - newWidth / 2,
+			top: center.y - newHeight / 2,
+			width: newWidth,
+			height: newHeight
+		};			
+		verifyViewBoxMove();
+		console.log(getViewBoxString());
+		gsap.to(svgMap, {
+			duration: 0.3,
+			attr: {
+				viewBox: getViewBoxString()
+			},
+			ease: "power2.inOut"
+		});	
+	}
+}
+
+function zoomOut(){
+	if(state == "nav"){
+		let center = {
+			x: currentViewBox.left + currentViewBox.width / 2,
+			y: currentViewBox.top + currentViewBox.height / 2
+		};
+		let rect = mapImage.getBoundingClientRect();
+		let ratio = rect.width / rect.height;
+		console.log(ratio);
+		let newWidth = currentViewBox.width + currentViewBox.width * zoomStrength;
+		let newHeight = newWidth / ratio;
+		if(newWidth > fullViewBox.width){
+			newWidth = fullViewBox.width;
+			newHeight = fullViewBox.height;
+		}
+		
+		
+		if(newHeight > fullViewBox.height){
+			newHeight = fullViewBox.height;
+			newWidth = fullViewBox.width;
+		}
+		currentViewBox = {
+			left: center.x - newWidth / 2,
+			top: center.y - newHeight / 2,
+			width: newWidth,
+			height: newHeight
+		};
+		verifyViewBoxMove();
+		console.log(getViewBoxString());
+		
+		gsap.to(svgMap, {
+			duration: 0.3,
+			attr: {
+				viewBox: getViewBoxString()
+			},
+			ease: "power2.inOut"
+		});		
+	}
+}	
